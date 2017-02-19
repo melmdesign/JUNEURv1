06d5768bcfe01ad71c33d19b6cb70e9ae3452579
melmdesign <liamm.designs@gmail.com>
2017-02-19 21:52:28 +0000

    LRG

--- Gemfile
+++ Gemfile
@@ -1,13 +1,15 @@
-# If you do not have OpenSSL installed, change
-# the following line to use 'http://'
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
 source 'https://rubygems.org'
 
+gem "middleman", "~>3.4.1"
+gem "contentful_middleman"
+
+# Live-reloading plugin
+gem "middleman-livereload"
+
 # For faster file watcher updates on Windows:
-gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
 # Windows does not come with time zone data
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-
-# Middleman Gems
-gem 'middleman', '~> 4.2'
-gem 'middleman-livereload', '~> 3.4'
+gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
--- config.rb
+++ config.rb
@@ -1,31 +1,44 @@
 ###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
+# page "/path/to/file.html", :layout => false
+#
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -34,11 +47,35 @@ end
 #   end
 # end
 
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
+  # For example, change the Compass output style for deployment
   # activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+
+
+activate :contentful do |f|
+  f.space         = { contentful: 'e3tkex9kqdfm'}
+  f.access_token  = ENV['7ec89f916560ccaaea0319417bbadfeffeafc010d3532f9c4dfd791ed82e9c97']
+  f.cda_query     = { content_type: 'jobs', include: 3 }
+  f.content_types = { jobs: 'jobs'}
 end
